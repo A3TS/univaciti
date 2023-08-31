afb06760de55dbb19547cf21873b9f08f24df2ca
A3TS <117208068+A3TS@users.noreply.github.com>
2023-08-31 10:01:06 +0100

    Update new.js

--- new.js
+++ new.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
 test('API Test', async () => {
-  const response = await axios.get(process.env.API_URL);
+  const response = await axios.get(https://pokeapi.co/api/v2/pokemon/ditto);
   expect(response.status).toBe(200);
 });
